03086d28edc4905204ad68c40d9bf8ae0549b1bd
Gia Kinh <giakinh2000@gmail.com>
2020-12-19 17:57:47 +0700

    done lan 1

--- appUser/urls.py
+++ appUser/urls.py
@@ -2,42 +2,12 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.auth import views as auth_views
 
 app_name = 'appUser'
 
 urlpatterns = [
     path('userInfo/', views.userInfo, name ='userInfo'),
     path('editInfo/', views.editInfo, name ='editInfo'),
-    path('change-password/',auth_views.PasswordChangeView.as_view(template_name='user/change-password.html',
-            success_url = '/'
-        ),
-        name='change_password'
-    ),
-    # Forget Password
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='user/password-reset/password_reset.html',
-             subject_template_name='user/password-reset/password_reset_subject.txt',
-             email_template_name='user/password-reset/password_reset_email.html',
-             success_url='/login/'
-         ),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='user/password-reset/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='user/password-reset/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='user/password-reset/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- saleProduct/urls.py
+++ saleProduct/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from home import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +28,35 @@ urlpatterns = [
     #register
     path('',include('register.urls')),
     #product
-    path('',include('appProduct.urls'))
+    path('',include('appProduct.urls')),
+    #auth
+     path('change-password/',auth_views.PasswordChangeView.as_view(template_name='user/change-password.html',
+            success_url = '/'
+        ),
+        name='change_password'
+    ),
+    # Forget Password
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+            template_name='user/password-reset/password_reset.html',
+            subject_template_name='user/password-reset/password_reset_subject.txt',
+            email_template_name='user/password-reset/password_reset_email.html',
+            success_url='/login/'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+            template_name='user/password-reset/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+            template_name='user/password-reset/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='user/password-reset/password_reset_complete.html'
+        ),
+        name='password_reset_complete'),
 ]
